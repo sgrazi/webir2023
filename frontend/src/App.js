efffe445eb57d41d92a3a7b4bcdf070d8f9b7b9f
trodriguez10 <95036319+trodriguez10@users.noreply.github.com>
2023-10-15 17:27:23 -0300

    Lyrics View

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,20 @@
 import React from "react";
 import { useState } from "react";
-import { TestComponent } from "./Components/TestComponent";
 import "./App.css";
+import "./Components/styles.css";
 import Navbar from "./Components/Navbar";
 import { LyricsView } from "./Views/LyricsView";
 import { SearchView } from "./Views/SearchView";
 
 function App() {
-  const [currentView, setCurrentView] = useState("");
-  let view;
-
-  switch (currentView) {
-    case "Search":
-      view = <SearchView />;
-      break;
-    case "Lyrics":
-      view = <LyricsView />;
-      break;
-    default:
-      view = <TestComponent />;
-      break;
-  }
+  const [currentView, setCurrentView] = useState("Lyrics");
 
   return (
     <div className="App">
       <Navbar className="navbar" setCurrentView={setCurrentView} />
-      <div className="main-content">{view}</div>
+      <div className="main-content">
+        {currentView === "Lyrics" ? <LyricsView/> : <SearchView/>}
+      </div>
     </div>
   );
 }
