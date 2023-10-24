1a9ae119399f5745f35b06e74247ec4136d61da0
William Tift <williamtift@outlook.com>
2023-10-24 10:00:27 -0300

    Pagination for elastic

--- frontend/src/Views/LyricsView.js
+++ frontend/src/Views/LyricsView.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import { Button, Box, TextField, Pagination, PaginationItem, MenuItem, FormHelperText, FormControl, Select } from "@mui/material";
 import axios from "axios";
 import { LyricsResultList } from "../Components/LyricsResultList";
 
 export function LyricsView() {
+  const maxAmmount = 10;
+
   const [searchQuery, setSearchQuery] = useState("");
   const [results, setResults] = useState(undefined);
   const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [currentItems, setCurrentItems] = useState(undefined);
+
+  const rangeValues = Array.from({ length: maxAmmount }, (_, i) => i + 1);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
     if (searchQuery !== "")
-    // Change the URL and set the results correctly
       axios
         .get(
           `http://localhost:8080/elastic/search?query=${searchQuery}`
@@ -26,12 +29,21 @@ export function LyricsView() {
         });
   };
 
+  useEffect(() => {
+    if (results) {
+      const indexOfLastItem = currentPage * pageSize;
+      const indexOfFirstItem = indexOfLastItem - pageSize;
+      const itemsToDisplay = results.slice(indexOfFirstItem, indexOfLastItem)
+      setCurrentItems(itemsToDisplay);
+    }
+  }, [currentPage, pageSize, results]);
+
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: '5%' }}>
       <Box component="form" onSubmit={handleSubmit} style={{ width: "50%", textAlign: "center" }}>
         <TextField
           focused
-          label="Buscar letras"
+          label="Buscar letras}"
           variant="outlined"
           value={searchQuery}
           color="success"
@@ -45,30 +57,36 @@ export function LyricsView() {
           Buscar
         </Button>
       </Box>
-      {results && 
+      {currentItems &&
         <div className="results-container">
           <div className="results-header">
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <FormHelperText style={{ color: "white" }}>
+                Cantidad por pagina:
+              </FormHelperText>
               <Select
                 size="small"
-                value={currentPage}
-                onChange={(e) => setCurrentPage(e.target.value)}
+                value={pageSize}
+                onChange={(e) => {
+                  setPageSize(e.target.value)
+                }}
                 style={{
                   background: "white",
                 }}
               >
-                <MenuItem value={1}>1</MenuItem>
+                {rangeValues.map((value) => (
+                  <MenuItem key={value} value={value}>
+                    {value}
+                  </MenuItem>
+                ))}
               </Select>
-              <FormHelperText style={{ color: "white" }}>
-                Select page
-              </FormHelperText>
             </FormControl>
           </div>
-        <LyricsResultList results={results} searchQuery={searchQuery} />
-        <Pagination
-            count={10}
-            page={currentPage}
-            onChange={(e) => setCurrentPage(e.target.value)}
+          <LyricsResultList results={currentItems} searchQuery={searchQuery} />
+          <Pagination
+            count={4} // Total number of pages
+            page={currentPage} // Current page
+            onChange={(event, page) => setCurrentPage(page)}
             style={{ display: "flex", justifyContent: "center" }}
             renderItem={(item) => (
               <PaginationItem {...item} style={{ color: "white" }} />
